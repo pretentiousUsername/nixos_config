2bee7b6f02d054964a9e287a6507f041e901a1f4
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-01-08 19:48:13 -0500

    add xclip

--- src/configuration.nix
+++ src/configuration.nix
@@ -226,6 +226,8 @@
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
 
+  programs.xclip.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
