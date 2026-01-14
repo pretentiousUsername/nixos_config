9f073690935d8a440ad10c61d03225af43b68031
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-01-13 23:14:10 -0500

    add gvfs to services

--- src/configuration.nix
+++ src/configuration.nix
@@ -118,7 +118,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -264,6 +264,7 @@
   # };
 
   # List services that you want to enable:
+  services.gvfs.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
