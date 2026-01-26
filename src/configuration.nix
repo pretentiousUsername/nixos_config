ab0bf4bf76b6049addd69b458164b2de1143c21f
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-01-26 14:04:53 -0500

    remove pulse from services

--- src/configuration.nix
+++ src/configuration.nix
@@ -110,7 +110,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
