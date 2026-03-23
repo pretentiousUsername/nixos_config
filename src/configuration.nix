43218cb26138cfbb3ce2af7a0557c44a7b3a20b5
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-03-23 00:19:00 -0400

    add xrandr

--- src/configuration.nix
+++ src/configuration.nix
@@ -205,6 +205,7 @@
    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     killall
+    xrandr
     neovim
     ranger
     ueberzug
